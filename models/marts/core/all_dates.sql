05b1af3dbedf5e111c2339b6b08db39001d340bb
Devashree Patel <devashree.patel@montrealanalytics.com>
2022-04-02 04:35:29 +0000

    dbt fundamentals complete and migrate legacy code for customer orders

--- models/marts/core/all_dates.sql
+++ models/marts/core/all_dates.sql
@@ -1,9 +1,11 @@
 {{config
 (materialized ="tables")}}
 
-{{dbt_utils.date_spine(
-datepart ="day"
-start_date =cast('2020-01-01' as date)
-end_date = cast('2020-12-31' as date)
-)
+{{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2020-01-01' as date)",
+    end_date="cast('2021-01-01' as date)"
+   )
 }}
+
+
