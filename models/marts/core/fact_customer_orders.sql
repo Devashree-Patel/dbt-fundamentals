803d6dc4057fa853d0b6b301667a560699dfa92a
Devashree Patel <devashree.patel@montrealanalytics.com>
2022-04-03 02:16:07 +0000

    Refactored

---
+++ models/marts/core/fact_customer_orders.sql
@@ -0,0 +1,64 @@
+WITH  customers as (
+
+    select * from {{source('jaffle_shop','customers')}}
+
+    ),
+orders as (
+
+ select * from {{source('jaffle_shop','orders')}}
+
+),
+payments as (
+
+ select * from {{source('jaffle_shop','payments')}}
+
+),
+
+completed_payments as (
+    select orderid as order_id, 
+    max(created) as payment_finalized_date, 
+    sum(amount) / 100.0 as total_amount_paid
+    from payments
+    where status <> 'fail'
+    group by 1
+),
+
+paid_orders as (
+        select orders.id as order_id,
+        orders.user_id    as customer_id,
+        orders.order_date AS order_placed_at,
+        orders.status AS order_status,
+        completed_payments.total_amount_paid,
+        completed_payments.payment_finalized_date,
+        customers.first_name    as customer_first_name,
+        customers.last_name as customer_last_name
+    FROM orders
+    left join completed_payments  ON orders.id = p.order_id
+left join customers  on orders.user_id = c.id ),
+
+customer_orders 
+    as (select c.id as customer_id
+        , min(order_date) as first_order_date
+        , max(order_date) as most_recent_order_date
+        , count(orders.id) as number_of_orders
+    from customers c 
+    left join orders
+    on orders.user_id = c.id 
+    group by 1)
+
+select
+    paid_orders.*,
+    row_number() over (order by paid_orders.order_id) as transaction_seq,
+    row_number() over (partition by customer_id order by paid_orders.order_id) as customer_sales_seq,
+    case when customer_orders.first_order_date = paid_orders.order_placed_at
+    then 'new'
+    else 'return' END as nvsr,
+    sum(total_amount_paid) over (
+        partition by paid_orders.customer_id
+        order by paid_orders.order_placed_at
+    )as customer_lifetime_value,
+    customer_orders.first_order_date as fdos
+    from paid_orders 
+    left join customer_orders on paid_orders.customer_id = customer_orders.customer_id
+    LEFT outer join 
+    order by order_id
